2650e715442a043800b3a4ee39ab0d55d3dd24cd
charles-afk <agudelo.charles@gmail.com>
2022-08-20 17:22:10 -0400

    background image

--- chakra/src/App.tsx
+++ chakra/src/App.tsx
@@ -91,6 +91,14 @@ function App() {
         gap={2} autoFlow="row dense">
         Grid
       </Grid>
+
+      {/** Background */}
+      <Box w="100%" h="1200px"
+        backgroundImage="url('https://images.ctfassets.net/sfnkq8lmu5d7/4Ma58uke8SXDQLWYefWiIt/3f1945422ea07ea6520c7aae39180101/2021-11-24_Singleton_Puppy_Syndrome_One_Puppy_Litter.jpg?w=1000&h=750&fl=progressive&q=80&fm=jpg')"
+        backgroundPosition="center"
+        backgroundRepeat="no-repeat"
+      />
+
     </div>
   );
 }
