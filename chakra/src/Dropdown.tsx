9f5e6553072ea82913fd46f9b5dff57f9ef2185d
charles-afk <agudelo.charles@gmail.com>
2022-08-27 17:01:04 -0400

    init dropdown

---
+++ chakra/src/Dropdown.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Box, Text, Flex, Grid, Button, SimpleGrid } from "@chakra-ui/react"
+
+const Dropdown = () => {
+  return (
+    <div>
+        <Flex>
+            Testing
+        </Flex>
+    </div>
+  )
+}
+
+export default Dropdown
---
+++ chakra/src/GradientTutorial.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Box, Text, Flex, Grid, Button, SimpleGrid } from "@chakra-ui/react"
+
+const GradientTutorial = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default GradientTutorial
